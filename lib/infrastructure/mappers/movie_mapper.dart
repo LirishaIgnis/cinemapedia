a83186e82381735d045cfc7098cbf76270af2b76
LirishaIgnis <drakorra@gmail.com>
2024-11-27 15:08:59 -0500

    Fin seccion 12 - Infraestructura Cinemapedia

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -15,7 +15,7 @@ class MovieMapper {
       popularity: moviedb.popularity,
       posterPath: ( moviedb.posterPath !='')
       ? 'https://image.tmdb.org/t/p/w500/${moviedb.posterPath}'
-       :'https://i.etsystatic.com/19289482/r/il/d3a4fb/4730259787/il_fullxfull.4730259787_8z2z.jpg',
+       :'no-poster',
       releaseDate: moviedb.releaseDate,
       title: moviedb.title,
       video: moviedb.video,
